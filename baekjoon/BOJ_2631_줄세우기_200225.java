81075ca845f78e2692cbf3c8274d422742fd92d9
YongJin Chun <43107012+sharhp21@users.noreply.github.com>
2020-02-28 14:56:50 +0900

    200228

--- baekjoon/BOJ_2631_줄세우기_200225.java
+++ baekjoon/BOJ_2631_줄세우기_200225.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
+// n - LIS
 public class BOJ_2631_줄세우기_200225 {
 	static int max = 0;
 	public static void main(String[] args) throws NumberFormatException, IOException {
